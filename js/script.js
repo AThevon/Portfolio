ee8b51c4c7b2381c5c738b46e98bc4b6acd1a874
AThevon <adrienthevon@gmail.com>
2023-02-21 19:48:47 +0100

    seventh commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 //* SETTING DU LANDING 
-
+if (window.innerWidth < 1025) {
+    console.log('coucou');
+    document.body.style.overflow = 'auto';
+}
 // Responsive header
 const myH2 = document.querySelector('.title-h2');
 
@@ -20,9 +23,9 @@ function handleMouseMove(event) {
     const height = window.innerHeight;
     const width = window.innerWidth;
 
-    // Creates angles (left, bottom) and (right, top)
+    // Création des angles (left, bottom) and (right, top)
     const yAxisDegree = event.pageX / width * 80 - 40;
-    const xAxisDegree = event.pageY / height * -1 * 20 + 20;
+    const xAxisDegree = event.pageY / height * -1 * 80 + 50;
 
     card.style.transform = `rotateY(${yAxisDegree}deg) rotateX(${xAxisDegree}deg)`;
 }
@@ -46,8 +49,7 @@ logoDev.addEventListener('click', function () {
     logoDevBg.style['transition'] = ".8s";
 
     // Mise en place de timers
-    var delay1 = 1000;
-    setTimeout(function () {
+    setTimeout(() => {
         movingCard.style.height = "0";
         movingCard.style.width = "0";
         navBar.style.gap = "0";
@@ -55,16 +57,14 @@ logoDev.addEventListener('click', function () {
         logoDev.style.width = "0";
         logoDevBg.style.height = "0";
         logoDevBg.style.width = "0";
-    }, delay1);
+    }, 1000);
 
-    var delay2 = 1500;
-    setTimeout(function () {
+    setTimeout(() => {
         navBar.style['height'] = "8rem";
         document.body.style['overflow-y'] = 'auto';
-    }, delay2);
+    }, 1500);
 
-    var delay3 = 2200;
-    setTimeout(function () {
+    setTimeout(() => {
         document.querySelector('.card-1').setAttribute('id', 'card-1')
         document.querySelector('.card-2').setAttribute('id', 'card-2')
         document.querySelector('.card-3').setAttribute('id', 'card-3')
@@ -72,29 +72,43 @@ logoDev.addEventListener('click', function () {
         document.querySelector('.first-name').setAttribute('id', 'first-name-fade')
         document.querySelector('.last-name').setAttribute('id', 'last-name-fade')
         document.querySelector('.job').setAttribute('id', 'job-fade')
-    }, delay3);
+    }, 2200);
 
-    var delay4 = 3000;
-    setTimeout(function () {
+    setTimeout(() => {
         document.querySelector('.title').classList.add('title-anim');
         document.querySelector('.left-bracket').classList.add('left-bracket-anim');
         document.querySelector('.right-bracket').classList.add('right-bracket-anim');
-    }, delay4);
+    }, 3000);
 
-    var delay5 = 3800;
-    setTimeout(function () {
+    setTimeout(() => {
         document.querySelector('.title-h2').textContent = '.portfolio';
-    }, delay5);
+    }, 3800);
 
-    var delay6 = 5000;
-    setTimeout(function () {
+    setTimeout(() => {
         document.querySelector('.btn-nav').style['opacity'] = '1';
-    }, delay6);
+    }, 5000);
 
 })
 
 
-//! #region CARDS HERO
+
+// ANIMATION DU LOGO DES CARDS EXP ON CLICK
+
+document.querySelectorAll('.card-exp').forEach((el) => {
+    el.addEventListener('click', logoAnim)
+})
+
+function logoAnim(e) {
+    const figure = e.currentTarget.querySelector('figure');
+    figure.classList.add('figure-onclick');
+    setTimeout( () => {
+        figure.classList.remove('figure-onclick');
+    }, 400);
+
+}
+
+
+//! CARDS HERO
 
 gsap.to(".card-out", {
     scrollTrigger: {
@@ -108,26 +122,6 @@ gsap.to(".card-out", {
     x: -2000,
     y: 600,
 });
-
-//! #endregion
-
-// ANIMATION ALEATOIRE DES CARDS ON CLICK
-
-document.querySelectorAll('.card-exp').forEach((el) => {
-    el.addEventListener('click', randomAnim)
-})
-
-let allAnims = ["anim-card-1", "anim-card-2", "anim-card-3", "anim-card-4", "anim-card-5", "anim-card-6", "anim-card-7", "anim-card-8"];
-
-function randomAnim() {
-    let currentId = this.id;
-    let randomId = allAnims[Math.floor(Math.random() * allAnims.length)];
-    if (currentId === randomId) {
-        randomAnim();
-    } else {
-        this.id = randomId;
-    }
-}
 
 
 //! SWIPERS.JS 
